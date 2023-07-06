7679c19ae47188463b071a2d54653ad8dc566c94
Anshu kumar <95128943+anshukumar21@users.noreply.github.com>
2023-07-06 10:57:19 +0530

    Create height-of-tree.cpp

---
+++ Tree/height-of-tree.cpp
@@ -0,0 +1,11 @@
+int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        int left = maxDepth(root -> left);
+        int right = maxDepth(root -> right);
+
+        int ans = max(left,right) + 1;
+
+        return ans;
+}
